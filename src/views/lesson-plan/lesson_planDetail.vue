d8e4227260ab9f54ed7cbf7e7d888dbaf62bbf22
wjh19920802 <676914741@qq.com>
2018-06-07 10:07:24 +0800

    1

--- src/views/lesson-plan/lesson_planDetail.vue
+++ src/views/lesson-plan/lesson_planDetail.vue
@@ -168,7 +168,7 @@
               >
                 <Button type="primary" size="large" style="float: left;">导入课表Excel</Button>
               </Upload>-->
-              <input type="file" ref="upload_file" style="display: none;">
+              <input type="file" ref="upload_file" style="display: none;" @change="uploadExcel">
               <Button @click="uploadFile">
                 上传Excel
               </Button>
@@ -1464,6 +1464,48 @@
       uploadFile() {
         // debugger
         this.$refs.upload_file.click()
+
+
+      },
+      uploadExcel () {
+        var fileObj = document.getElementById("FileUpload").files[0]; // js 获取文件对象
+        if (typeof (fileObj) == "undefined" || fileObj.size <= 0) {
+          alert("请选择图片");
+          return;
+        }
+        var formFile = new FormData();
+        formFile.append("action", "UploadVMKImagePath");
+        formFile.append("file", fileObj); //加入文件对象
+
+        //第一种  XMLHttpRequest 对象
+        //var xhr = new XMLHttpRequest();
+        //xhr.open("post", "/Admin/Ajax/VMKHandler.ashx", true);
+        //xhr.onload = function () {
+        //    alert("上传完成!");
+        //};
+        //xhr.send(formFile);
+
+        //第二种 ajax 提交
+
+        var data = formFile;
+        $.ajax({
+          url: "/Admin/Ajax/VMKHandler.ashx",
+          data: data,
+          type: "Post",
+          dataType: "json",
+          cache: false,//上传文件无需缓存
+          processData: false,//用于对data参数进行序列化处理 这里必须false
+          contentType: false, //必须
+          success: function (result) {
+            alert("上传完成!");
+          },
+        })
+
+        this.$http({
+          method:'post',
+          url:this.$store.app.state.baseUrl + course/gerCourseTableByExcel,
+
+        })
       }
     },
     mounted() {
